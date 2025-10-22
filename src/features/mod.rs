53f222ae3bdf2d9aeeccac1ebb610c93e3c79ec9
Jack Nash <jacknash42@gmail.com>
2025-10-22 10:16:48 +1000

    html templating

--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,11 +1,27 @@
-use axum::{Router, http::StatusCode, response::IntoResponse};
+use axum::{
+    Router,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+};
+use maud::html;
 
-use crate::app::server::AppState;
+use crate::{app::server::AppState, utils::WebPageBuilder};
 
 pub fn router() -> Router<AppState> {
     Router::new().fallback(handle_404)
 }
 
 async fn handle_404() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "Not Found")
+    let page = WebPageBuilder::new()
+        .body(html! {
+            h1 {
+                "Not Found"
+            }
+            div {
+                "Requested page does not exist"
+            }
+        })
+        .build();
+
+    (StatusCode::NOT_FOUND, Html(page.into_string()))
 }
---
+++ src/utils/mod.rs
@@ -0,0 +1,53 @@
+use maud::{Markup, html};
+
+#[derive(Debug)]
+pub struct WebPageBuilder {
+    pub title: String,
+    pub subtitle: Option<String>,
+    pub body: Markup,
+}
+
+impl WebPageBuilder {
+    pub fn new() -> Self {
+        Self {
+            title: "Walrus".into(),
+            subtitle: None,
+            body: html! {},
+        }
+    }
+
+    pub fn title(mut self, title: impl Into<String>) -> Self {
+        self.title = title.into();
+        self
+    }
+
+    pub fn subtitle(mut self, subtitle: Option<impl Into<String>>) -> Self {
+        self.subtitle = match subtitle {
+            Some(s) => Some(s.into()),
+            _ => None,
+        };
+        self
+    }
+
+    pub fn body(mut self, body: impl Into<Markup>) -> Self {
+        self.body = body.into();
+        self
+    }
+
+    pub fn build(self) -> Markup {
+        let gen_title = match self.subtitle {
+            Some(sub) => format!("{} | {}", sub, self.title),
+            _ => self.title,
+        };
+
+        html! {
+            head {
+                meta charset="utf-8";
+                title { (gen_title) }
+            }
+            body {
+               (self.body)
+            }
+        }
+    }
+}
